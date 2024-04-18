1352b90513cdb337b2aaad0f52d9430585c75e75
Vijayaprm <vijayapratapreddy.m@gmail.com>
2024-04-18 22:45:43 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//index.js 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from 'react'; 
+import ReactDOM from 'react-dom/client'; 
+import './index.css'; 
+import App from './App'; 
+import { Provider } from "react-redux"
+import store from './redux/store'; 
+import { ToastContainer } from 'react-toastify'; 
+import 'react-toastify/dist/ReactToastify.css'; 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+root.render( 
+	<React.StrictMode> 
+		<ToastContainer bodyStyle={{ fontFamily: "Roboto" }} /> 
+		<Provider store={store}> 
+			<App /> 
+		</Provider> 
+	</React.StrictMode> 
+); 
